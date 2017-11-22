88261bae4b241e45c5823b310d8c95273d068bc9
iain <iain.diamond@gmail.com>
2017-11-22 14:36:10 +0000

    Process Ideas form input

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
 const ideaRoutes = require('./routes/idea')
 const db = require('./models')
-
 const app = express()
 
+// Database Schemas
 const idea = db.Idea
 
 // Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.engine('handlebars', hbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
--- routes/idea.js
+++ routes/idea.js
@@ -8,6 +8,32 @@ router.get('/', (req, res) => {
   })
 })
 
+router.post('/', (req, res) => {
+  let errorMessages = {
+    title: 'Please enter a title.',
+    details: 'Please enter some details',
+  }
+  let { title, details } = req.body
+  let errors = []
+
+  if (!title) {
+    errors = errors.concat([{ message: errorMessages.title }])
+  }
+  if (!details) {
+    errors = errors.concat([{ message: errorMessages.details }])
+  }
+
+  if (errors.length > 0) {
+    res.render('ideas/add', {
+      errors,
+      title,
+      details,
+    })
+  } else {
+    res.sendStatus(200)
+  }
+})
+
 router.get('/add', (req, res) => {
   res.render('ideas/add')
 })
